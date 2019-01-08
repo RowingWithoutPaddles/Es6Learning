252177a6a7eac42080df5bbcc6f48afd29fde58d
Army <1105601720@qq.com>
2019-01-09 00:25:48 +0800

    init commit

---
+++ src/components/Es6Learning.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+      <p><button @click="destructuring">变量的解构赋值</button></p>
+      <p><button>变量的解构赋值</button></p>
+  </div>
+</template>
+
+<script>
+import {es6_destructuring} from '@/js/destructuring.js'
+
+export default {
+    methods: {
+        destructuring() {
+            es6_destructuring()
+        },
+    }
+};
+</script>
+
+<style scoped>
+.main {
+  text-align: center;
+  margin: 0 auto;
+}
+</style>
+
+
---
+++ src/js/destructuring.js
@@ -0,0 +1,3 @@
+export function es6_destructuring() {
+    console.log('es6_destructuring')
+}
